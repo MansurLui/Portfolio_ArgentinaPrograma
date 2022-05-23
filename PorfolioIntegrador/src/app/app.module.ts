4d026d974d6e7356046ddf6734dc6102b33b60fc
MansurLui <agustinmansur@gmail.com>
2022-05-23 12:46:56 -0300

    portfolio v1.0

--- PorfolioIntegrador/src/app/app.module.ts
+++ PorfolioIntegrador/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    RedesSocialesComponent,
+    FooterComponent,
+    BodyComponent,
+    AcercaDeComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
